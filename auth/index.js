13d3ef7f3985866c447eb30a0bb3850251850a3c
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-08-07 12:26:09 -0600

    fixing queries

--- auth/index.js
+++ auth/index.js
@@ -8,14 +8,14 @@ var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 
 router.post('/signup', function(req, res, next){
-  queries.findUserByUserName(req.body.userName)
+  queries.findUserByUserName(req.query.userName)
   .then(function(user){
     if(user){
       res.json({
-        error : "user already makes list bitch"
+        error : "user already exist try another name"
       });
     }else {
-      auth.createUser(req.body)
+      auth.createUser(req.query)
       .then(function(user){
         res.json({
           message : 'you are a new user yay'
@@ -26,9 +26,9 @@ router.post('/signup', function(req, res, next){
 });
 
 router.post('/login', function(req, res, next){
-  queries.findUserByUserName(req.body.userName)
+  queries.findUserByUserName(req.query.userName)
   .then(function(user){
-    var plainTextPassword = req.body.password;
+    var plainTextPassword = req.query.password;
     if(user && bcrypt.compareSync(plainTextPassword, user.password)){
       jwt.sign(user, process.env.TOKEN_SECRET, {expiresIn: '1d'}, function(err, token){
         console.log(token);
